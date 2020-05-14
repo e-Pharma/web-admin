2ae99af96231ed3c9edf09b22a872822ea2891e5
Sankha Jayalath <sankha.rc@gmail.com>
2020-05-14 18:34:39 +0530

    angular routing

--- e-Pharma-admin/src/app/app.module.ts
+++ e-Pharma-admin/src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
-
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UpgradeComponent } from './upgrade/upgrade.component';
+import { TypographyComponent } from './typography/typography.component';
+import { TableListComponent } from './table-list/table-list.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { MapsComponent } from './maps/maps.component';
+import { IconsComponent } from './icons/icons.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+    UpgradeComponent,
+    TypographyComponent,
+    TableListComponent,
+    NotificationsComponent,
+    MapsComponent,
+    IconsComponent,
+    DashboardComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    AdminLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ e-Pharma-admin/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminLayoutComponent } from './admin-layout.component';
+
+
+
+@NgModule({
+  declarations: [AdminLayoutComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminLayoutModule { }
