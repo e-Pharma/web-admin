95917a435c2004e62fb747c6ae617d7e06cc9303
NipuniYR <nipuniyr@gmail.com>
2020-06-22 03:11:04 +0530

    Client module - view Client data retrieved

--- e-Pharma-admin/src/app/services/client/client.service.ts
+++ e-Pharma-admin/src/app/services/client/client.service.ts
@@ -1,26 +1,59 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { baseURL } from 'app/shared/baseurl';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  //change to baseURL (app/shared) LATER ************
-  baseURI:string = 'http://localhost:3000/admin/';
+  //baseURI:string = 'http://localhost:3000/admin/';
+  headers = new HttpHeaders().set('Content-Type','application/json')
 
   constructor(private http: HttpClient) { }
 
   //get verified clients
   getVerifiedClients(){
-    return this.http.get(this.baseURI+'verifiedClients');
-    //return this.http.get(baseURL+'admin/verifiedClients');
+    //return this.http.get(this.baseURI+'verifiedClients');
+    return this.http.get(baseURL+'admin/verifiedClients');
   }
 
   //get not verified clients
   getNotVerifiedClients(){
-    return this.http.get(this.baseURI+'notVerifiedClients');
-    //return this.http.get(baseURL+'admin/notVerifiedClients');
+    //return this.http.get(this.baseURI+'notVerifiedClients');
+    return this.http.get(baseURL+'admin/notVerifiedClients');
   }
+
+  getClient(id:String){
+    //return this.http.get(this.baseURI+'client/'+id).pipe(
+    //   map((res:Response)=>{
+    //     return res || {}
+    //   }),
+    //   catchError(this.errorMgmt)
+    // );
+    return this.http.get(baseURL+'admin/client/'+id).pipe(
+      map((res:Response)=>{
+        return res || {}
+      }),
+      catchError(this.errorMgmt)
+    );
+  }
+
+  //Error handling
+  errorMgmt(error:HttpErrorResponse){
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent){
+      //Get client-side error
+      errorMessage = error.error.message;
+    }
+    else{
+      //Get server-side error
+      errorMessage ='Error Code: ${error.status}\nMessage:${error.message}';
+      console.log(errorMessage);
+      return throwError(errorMessage);
+    }
+  }
+
 }
--- e-Pharma-admin/src/app/view-client/view-client.component.ts
+++ e-Pharma-admin/src/app/view-client/view-client.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ClientService } from 'app/services/client/client.service';
 
 @Component({
   selector: 'app-view-client',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-client.component.css']
 })
 export class ViewClientComponent implements OnInit {
+  Client:any = [];
 
-  constructor() { }
+  constructor(
+    private ClientServeice:ClientService,
+    private route:ActivatedRoute
+    ) {
+      let id = this.route.snapshot.paramMap.get('id');
+      this.readClient(id);  
+  }
 
   ngOnInit(): void {
+  }
+
+  readClient(id){
+    this.ClientServeice.getClient(id).subscribe((data)=>{
+      console.log("Hello from view Client")
+      console.log(data);
+      this.Client=data;
+      console.log(this.Client)
+    })
   }
 
 }
